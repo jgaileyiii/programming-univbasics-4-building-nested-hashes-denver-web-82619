a019af3fb3b62fb88c49fabe721a398fcf31d82c
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-17 20:49:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,9 +8,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {railroads: {pieces: 4, rent_in_dollars: {key1:[], key2:[], key3:[],key4:[]}, {names:[]}
-  }}
+  {railroads: {pieces: 4, rent_in_dollars: {key1:[], key2:[], key3:[],key4:[]}, names:[]}
+  }
+}
 end
+
 def monopoly_with_fourth_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
