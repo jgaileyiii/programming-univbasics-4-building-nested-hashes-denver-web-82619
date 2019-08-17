1fd4e2e13a50649d567cff4fa3bcb9c72f634cfe
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-17 20:43:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,8 +8,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {railroads: {pieces: 4, rent_in_dollars: {},{},{},{}
-    }}
+  {railroads: {pieces: 4, rent_in_dollars: {{},{},{},{}
+    }}}
  end
 
 def monopoly_with_fourth_tier
